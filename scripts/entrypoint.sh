597bdb3ab0eca4b55dbf32dd3854345700fed822
Dan Skaggs <dan@skaggsfamily.ws>
2020-04-11 10:24:28 -0400

    Added /bin/bash command to scheduled run type to keep container open. Feels kinda hacky.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,9 @@
 set -e
 
 if [[ -z "${SCHEDULE}" ]]; then
-    organize run
+    organize run >> /var/log/organize/organize.log 2>&1
   else
     (crontab -l 2>/dev/null; echo "$SCHEDULE /usr/local/bin/organize run >> /var/log/organize/organize.log 2>&1") | crontab -
     service cron restart
+    /bin/bash
 fi
-
-exec "$@"
