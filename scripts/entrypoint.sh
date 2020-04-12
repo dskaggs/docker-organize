efa7d184944e489aeaaea287940d679537f587ac
Dan Skaggs <dan@skaggsfamily.ws>
2020-04-12 19:06:30 -0400

    Added /bin/bash command to scheduled run option. Feels hacky.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,7 @@ if [ $# -eq 1 ]
   then
     (crontab -l 2>/dev/null; echo "$1 /usr/local/bin/organize run >> /var/log/organize/organize.log 2>&1") | crontab -
     service cron restart
+    /bin/bash
   else
 	  organize run && exit 0
 fi
